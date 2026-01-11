17f411cea4a2359e192267434d0d6062d705ad26
COPICEK <tuondra@seznam.cz>
2026-01-11 23:39:55 +0100

    jsem cooked

---
+++ Zkouška/cif_soucet/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+
+/*
+ * Napište kompletní program v jazyce C++, který jako
+ * parametr z příkazové řádky převezme číslo a v případě
+ * , že se jedná o celočíselnou hodnotu, určí ciferný
+ * součet tohoto čísla (tj. například pro zadané číslo
+ * –437 vypočte 4+3+7=14). Pokud parametr nebude zadán,
+ program nic nevypíše. Pokud jako parametr nebude
+ předáno celé číslo, program vypíše odpovídající
+ chybovou hlášku do standardního chybového výstupu.
+ */
+
+
+
+
+int main() {
+
+}
--- Zkouška/procvic_09/main.cpp
+++ Zkouška/procvic_09/main.cpp
@@ -14,19 +14,88 @@ Body za vzhled (celé číslo 0-10
 Pro každou pizzu spočítej celkové skóre (Chuť + Vzhled).
 Najdi dvě pizzy s nejvyšším celkovým skóre.
 Vypiš názvy těchto dvou vítězů.
- */
+
 
 
 int main() {
     string name;
     int chut,vzhled;
-
-    string nejNazev = "";
-    string druhynejNazev ="";
+    int celkem =0;
+    string nejNazev;
+    string druhynejNazev;
+    int nejcelkem = 0;
+    int druhanejcelkem =0;
 
     while(cin>>name>>chut>>vzhled) {
-        int celkem = chut+vzhled;
+        celkem = chut+vzhled;
+
+
+        if(celkem > nejcelkem) {
+            druhynejNazev = nejNazev;
+            druhanejcelkem = nejcelkem;
+
+            nejcelkem =celkem;
+            nejNazev = name;
+
+        }
+        else if(celkem>druhanejcelkem) {
+            druhanejcelkem = celkem;
+            druhynejNazev = name;
+        }
     }
-    
+    cout << "1. misto: " << nejNazev << " (" << nejcelkem << " bodu)" << endl;
+    cout << "2. misto: " << druhynejNazev << " (" << druhanejcelkem << " bodu)" << endl;
+
+    return 0;
+}
+*/
+
+/*
+Zadání: Na standardním vstupu jsou
+záznamy o hráčích v turnaji. Počet hráčů není znám (čti do konce vstupu).
+Každý řádek obsahuje:
+Přezdívka hráče (jedno slovo, např. "Killer123")
+Počet zabití (kills - celé číslo)
+Počet úmrtí (deaths - celé číslo)
+Úkol:Pro každého hráče spočítej skóre podle vzorce:
+skore = (kills * 10) - (deaths * 5). (Tedy za zabití dostane 10 bodů,
+za smrt se mu 5 odečte).
+Najdi dva hráče s nejvyšším skóre.
+Vypiš přezdívky a skóre těchto dvou vítězů.
+ */
+
+
+int main() {
+    string nickname;
+    int kills;
+    int deaths;
+
+
+
+    string nejNazev;
+    string druhynejNazev;
+    int nejcelkem =0;
+    int druhynejcelkem =0;
+
+    while(cin>>nickname>>kills>>deaths) {
+         int skore = (kills*10) - (deaths*5);
+
+
+        if(skore > nejcelkem) {
+            druhynejcelkem = nejcelkem;
+            druhynejNazev = nejNazev;
+
+            nejNazev = nickname;
+            nejcelkem =skore;
+        }
+        else if(skore>druhynejcelkem) {
+            druhynejcelkem = skore;
+            druhynejNazev = nickname;
+        }
+
+    }
+
+    cout <<"1. " << nejNazev << " (" <<nejcelkem <<")" <<endl;
+    cout <<"2. " << druhynejNazev <<" (" << druhynejcelkem<<")" <<endl;
 
 }
