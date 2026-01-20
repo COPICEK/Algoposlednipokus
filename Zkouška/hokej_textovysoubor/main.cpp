e3c4932504159d064c814525007c185fa438f766
COPICEK <tuondra@seznam.cz>
2026-01-20 14:19:37 +0100

    dodelany priklad

--- Zkouška/hokej_textovysoubor/main.cpp
+++ Zkouška/hokej_textovysoubor/main.cpp
@@ -1,21 +1,51 @@
 #include <iostream>
 using namespace std;
-
+#include <fstream>
 /*
 V textovém souboru „hokej.liga“ v aktuálním adresáři je na
 každém řádku dvojice údajů o hokejistech: identifikační řetězec (10 znaků) a
 počet vstřelených branek. Pokud je soubor v pořádku, přečtěte z něj všechny
 údaje a vypište na standardní výstup seznam hráčů,
 kteří se netrefili ani jednou a jednoho hráče s největším počtem vstřelených
-branek. Nebude-li soubor zpracovatelný,
+branek.
+Nebude-li soubor zpracovatelný,
 vypište odpovídající chybové hlášení a nastavte nenulový výstupní kód programu.
 
 */
 
 
 
-
-
 int main() {
+    string id;
+    int goly;
+    int mingoly = 0;
+    int maxgoly =0;
+    string maxjmeno = "";
 
+    ifstream soubor("hokej.liga");
+
+    if(!soubor) {
+        cout<<"soubor se nepodarilo otevrit" <<endl;
+        return 12;
+    }
+
+    if(soubor.is_open()) {
+        while(soubor >> id >>goly) {
+            if(goly == mingoly) {
+                cout<<"tito hraci nedali ani jeden gol"<< id <<endl;
+            }
+            else if( goly > maxgoly) {
+                maxgoly = goly;
+                maxjmeno = id;
+            }
+        }
+        cout<<"Hrac s nejvice goly: " << maxjmeno << maxgoly <<endl;
+    }
+
+    else {
+        cout<<"soubor je prazdny" <<endl;
+        return 2;
+    }
+    soubor.close();
+    return 0;
 }
